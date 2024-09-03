cdd390037de1fde3d108a48fd349979af019b055
Marcelle de Assis <marcelledassis@gmail.com>
2024-09-03 18:04:58 -0300

    atualização curso js

---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Marcelle "de Assis"'); // String = Texto
+console.log("Marcelle 'de Assis'");// Number = Número
+console.log(`'Marcelle' "de Assis"`);
+
+console.log(35, 15.85, "'Marcelle de Assis'");
+
+console.log('Meu nome é "Marcelle". Estou aprendendo JavaScript às', 15, "da tarde.")
---
+++ aula4/comentarios.js
@@ -0,0 +1,9 @@
+// Escreva um comentário
+console.log('Hello World!');
+console.log('Linha 1'); //Aqui vai ter outro comnetário
+console.log('Linha 2');
+//Podemos comentar em qualquer lugar//
+console.log('Linha 3');
+//Diferente de html e css, comentários em JS não tem o asteristico * //
+console.log('Linha 4');
+//Na exibição do código comentários são ignorados//
