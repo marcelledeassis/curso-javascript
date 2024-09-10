8943fd6c39a1beaf8aa9c020f5cad19480ea903a
Marcelle de Assis <marcelledassis@gmail.com>
2024-09-10 16:16:07 -0300

    atualizei a aula7

--- aula7/index.js
+++ aula7/index.js
@@ -1,14 +1,11 @@
 //CONSTANTES e VARIÁVEIS
-/*O que são variáveis e constantes?
-Variáveis: São como caixas que podem guardar diferentes valores ao longo do tempo. O valor dentro da caixa pode mudar quando quisermos.
-
-Constantes: São como caixas que só podem ser preenchidas uma vez. Uma vez que você coloca algo dentro, você não pode mais trocar.
-
-Para criar uma variável, usamos let. Para criar uma constante, usamos const.
-Usamos o sinal de igual (=) para atribuir um valor a uma variável ou constante.
-Podemos usar o nome da variável ou constante em qualquer lugar do código para acessar o valor que ela armazena.
+/*
+Constantes: Valores que não mudam. Use const.
+Variáveis: Valores que podem mudar. Use let.
+Tipos de dados: Os principais são number e string.
+Operações: Cuidado com a soma de números e strings.
+Verificando o tipo: Use typeof.
 */
-
 //Criando constantes
 const primeiroNumero = 5
 const segundoNumero = 10
