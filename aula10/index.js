4ff794a13e15caa5e2f6cc62b36b054ec50f580e
Marcelle de Assis <marcelledassis@gmail.com>
2024-09-12 12:35:50 -0300

    criei a aula10

---
+++ aula10/index.js
@@ -0,0 +1,18 @@
+//TIPOS DE DADOS PRIMITIVOS
+//string, number, undefined, boolean
+//undefined = indefinido, não aponta pra lugar nenhum na memória
+//null = nulo -> não aponta pra local nenhum na memória. desconfigura uma variavel.
+// diferença entre null e undefined
+// null -> fala explicitamente para essa variavel nao apontar p nenhum local na minha memoria
+// undefined -> não utilizo esse valor. quem faz isso é o proprio js. é automatico.
+//boolean = true, false (lógico)
+
+const nome = 'Marcelle'; //string
+const nome1 ="Marcelle"; //string
+const nome2 = `Marcelle`; //string 
+const num1 = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; //undefined
+const sobrenomeAluno = null;
+const aprovado = true;
+
