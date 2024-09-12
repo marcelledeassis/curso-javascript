d64127ed914fa2704e3331fdec81ceec2ce6bf56
Marcelle de Assis <marcelledassis@gmail.com>
2024-09-12 18:01:29 -0300

    criei a aula11

---
+++ aula11/index.js
@@ -0,0 +1,22 @@
+/**
+ * Operadores Aritméticos
+Operações básicas: Soma (+), subtração (-), multiplicação (*), divisão (/), resto da divisão (%) e potenciação (**).
+Ordem de precedência: As operações dentro de parênteses são executadas primeiro, seguidas pela potenciação, multiplicação, divisão e resto da divisão, e por fim, soma e subtração.
+
+ * Operadores de Incremento
+++: Aumenta o valor de uma variável em 1.
+Pós-incremento (contador++): O valor da variável é usado na expressão e depois incrementado.
+Pré-incremento (++contador): O valor da variável é incrementado e depois usado na expressão.
+Operador de decremento (--): Diminui o valor de uma variável em 1.
+
+ * Operadores de Atribuição
+**+=, -=, *=, /=, %=, =: Combinam uma operação aritmética com a atribuição. Por exemplo, contador += 5 é equivalente a contador = contador + 5.
+
+ * Conversão de Tipos
+parseInt(): Converte uma string para um número inteiro.
+parseFloat(): Converte uma string para um número de ponto flutuante.
+Number(): Converte um1 valor para um número.
+
+---NaN: Representa "Not a Number" e ocorre quando se tenta realizar uma operação matemática com valores não numéricos.
+----Tipos de dados: É importante ter cuidado com os tipos de dados envolvidos nas operações, pois tentar realizar operações entre tipos diferentes pode levar a resultados inesperados.
+ */
